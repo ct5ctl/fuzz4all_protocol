12caba6f133e1e12cbf396496dd3e85b9fc33982
brutalsavage <stevenxia990430@gmail.com>
2023-02-26 22:45:56 -0600

    feat: added basic gcc optimization oracle

--- target/GCC.py
+++ target/GCC.py
@@ -8,23 +8,51 @@ class GCCTarget(Target):
 
     def validate_individual(self, filename):
         self.logger.logo("Validating {} ...".format(filename))
-        exit_code = subprocess.run("gcc {} -o testbed/out".format(filename), shell=True, stdout=subprocess.DEVNULL,
-                                   stderr=subprocess.DEVNULL)
+        exit_code = subprocess.run("gcc {} -o testbed/out".format(filename), shell=True,
+                                   capture_output=True,
+                                   text=True)
         if exit_code.returncode != 0:
             return FResult.FAILURE, "failed to compile"
 
         try:
-            exit_code = subprocess.run("./testbed/out",
+            # TODO: maybe solve alot of user input related code, its causing many timeouts
+            base_code = subprocess.run("./testbed/out",
                                        shell=True,
-                                       stdout=subprocess.DEVNULL,
-                                       stderr=subprocess.DEVNULL,
+                                       capture_output=True,
+                                       text=True,
                                        timeout=self.timeout if self.timeout else None)
         except subprocess.TimeoutExpired as te:
             subprocess.run(["ps -ef | grep 'testbed/out' | grep -v grep | awk '{print $2}'"], shell=True)
             subprocess.run(["ps -ef | grep 'testbed/out' | grep -v grep | awk '{print $2}' | xargs -r kill -9"],
-                   shell=True)  # kill all tests thank you
+                           shell=True)  # kill all tests thank you
             return FResult.FAILURE, "timed out"
-        if exit_code.returncode != 0:
-            return FResult.ERROR, exit_code
+
+        if base_code.returncode != 0:
+            return FResult.ERROR, base_code.stderr
+
+        # optimization running
+        for op in ['O1', 'O2', 'O3']:
+            exit_code = subprocess.run("gcc {} -{} -o testbed/out{}".format(filename, op, op), shell=True,
+                                       capture_output=True,
+                                       text=True)
+            if exit_code.returncode != 0:
+                return FResult.ERROR, "{} failed to compile".format(op)
+            try:
+                op_code = subprocess.run("./testbed/out{}".format(op),
+                                           shell=True,
+                                           capture_output=True,
+                                           text=True,
+                                           timeout=self.timeout if self.timeout else None)
+            except subprocess.TimeoutExpired as te:
+                pname = "'testbed/out{}'".format(op)
+                subprocess.run(["ps -ef | grep " + pname + " | grep -v grep | awk '{print $2}'"], shell=True)
+                subprocess.run(["ps -ef | grep " + pname + " | grep -v grep | awk '{print $2}' | xargs -r kill -9"],
+                               shell=True)  # kill all tests thank you
+                return FResult.ERROR, "{} timed out".format(op)
+
+            if op_code.stdout != base_code.stdout:
+                return FResult.ERROR, "{} different results:\nNo Optimization: {}\nOptimization: {}"\
+                    .format(op, base_code.stdout, op_code.stdout)
 
         return FResult.SAFE, "its safe"
+
---
+++ target/tests/test_optimization.c
@@ -0,0 +1,35 @@
+// test optimization failure O2 results different than base gcc
+// taken from https://stackoverflow.com/questions/61370485/is-it-a-gcc-o2-optimization-bug-different-result-from-o1
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+    uint32_t A[4] = { 1, 2, 3, 4 };
+    float B[4] = { 0, 0, 0, 0 };
+    float C[4] = { 5, 6, 7, 8 };
+    int i;
+
+    // convert integer A to float B
+    for (i = 0; i < 4; i++) 
+        B[i] = (float)A[i];
+
+    // memory copy from B to C
+    uint32_t *src = (uint32_t*)(B);
+    uint32_t *dst = (uint32_t*)(C);
+    dst[0] = src[0];
+    dst[1] = src[1];
+    dst[2] = src[2];
+    dst[3] = src[3];
+
+#if 0
+    // open this to correct the error
+    __asm__("":::"memory");
+#endif
+
+    // print C, C should be [1.0, 2.0, 3.0, 4.0]
+    for (i = 0; i < 4; i++) 
+        printf("%f\n", C[i]);
+
+    return 0;
+}
