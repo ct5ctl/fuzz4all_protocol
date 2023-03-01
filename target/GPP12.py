e87a00d15a3ed13eba7665b28892f9948170f181
test <stevenxia990430@gmail.com>
2023-03-01 00:14:52 -0600

    feat: added gpp12 oracle

---
+++ target/GPP12.py
@@ -0,0 +1,44 @@
+import subprocess
+
+from target.base_target import Target
+from target.base_target import FResult
+
+
+class GPP12(Target):
+
+    def validate_individual(self, filename) -> (FResult, str):
+        self.logger.logo("Validating {} ...".format(filename))
+        exit_code = subprocess.run("g++ -c {} -std=c++23 -o testbed/out".format(filename), shell=True,
+                                   capture_output=True,
+                                   encoding="utf-8",
+                                   text=True)
+        if exit_code.returncode == 1:
+            return FResult.FAILURE, "failed to compile"
+        elif exit_code.returncode != 0:
+            return FResult.ERROR, exit_code.stderr
+
+        for op in ['O1', 'O2', 'O3']:
+            exit_code = subprocess.run("g++ -{} -c {} -std=c++23 -o testbed/out{}".format(op, filename, op), shell=True,
+                                       capture_output=True,
+                                       encoding="utf-8",
+                                       text=True)
+            if exit_code.returncode != 0:
+                return FResult.ERROR, exit_code.stderr
+
+        # check without -c option (+ linking)
+        exit_code = subprocess.run("g++ {} -std=c++23 -o testbed/out".format(filename), shell=True,
+                                   capture_output=True,
+                                   encoding="utf-8",
+                                   text=True)
+        if exit_code.returncode == 1:
+            return FResult.FAILURE, "failed to compile"
+        elif exit_code.returncode != 0:
+            return FResult.ERROR, exit_code.stderr
+
+        for op in ['O1', 'O2', 'O3']:
+            exit_code = subprocess.run("g++ {} -std=c++23 -{} -o testbed/out{}".format(filename, op, op), shell=True,
+                                       capture_output=True,
+                                       encoding="utf-8",
+                                       text=True)
+            if exit_code.returncode != 0:
+                return FResult.ERROR, "{} failed to compile".format(op)
