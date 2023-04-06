8c29c063ff644d9feaf3a6c75cf730141b3191f2
steven <stevenxia990430@gmail.com>
2023-04-06 00:13:58 -0500

    fix: more fix with smt oracle

--- target/SMT/SMT.py
+++ target/SMT/SMT.py
@@ -7,6 +7,11 @@ from target.base_target import FResult
 def _check_sat(stdout):
     sat = ""
     for x in stdout.splitlines():
+        if x.strip() == "unknown" or x.strip() == "unsupported":
+            sat = "unknown"
+            return sat
+
+    for x in stdout.splitlines():
         if x.strip() == "unsat" or x.strip() == "sat":
             sat = x.strip()
             break
@@ -38,6 +43,8 @@ def _check_cvc5_parse_error(stdout):
 # remove logic set which can lead to parse errors
 def clean_logic(code):
     clean_code = "\n".join([x for x in code.splitlines() if not x.startswith("(set-logic")])
+    clean_code = "\n".join([x for x in clean_code.splitlines() if not x.startswith("(set-option :")])
+    clean_code = "\n".join([x for x in clean_code.splitlines() if not x.startswith("(get-proof)")])
     return clean_code
 
 
@@ -57,7 +64,7 @@ class SMTTarget(Target):
         self.write_back_file(clean_logic(code))
         try:
             cvc_exit_code = subprocess.run(
-                    "cvc5-Linux -m -i -q --check-models --lang smt2 temp{}.smt2".format(self.CURRENT_TIME),
+                    "cvc5 -m -i -q --check-models --lang smt2 temp{}.smt2".format(self.CURRENT_TIME),
                     shell=True, capture_output=True, text=True,
                     timeout=5)
         except subprocess.TimeoutExpired as te:
@@ -79,32 +86,42 @@ class SMTTarget(Target):
                            shell=True)  # kill all tests thank you
             return FResult.TIMED_OUT, "Z3 Timed out"
 
-        # failed compilation
-        if z3_exit_code.returncode == 1 and cvc_exit_code.returncode == 1:
-            return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout, cvc_exit_code.stdout)
-
         # first grab the sat results, because some error can be tolerable for sat solving actually
         z3_sat = _check_sat(z3_exit_code.stdout)
         cvc5_sat = _check_sat(cvc_exit_code.stdout)
         if (z3_sat != "" and cvc5_sat != "") and z3_sat != cvc5_sat:
-            return FResult.ERROR, "Different SAT Results. Z3: {} CVC5: {}".format(z3_sat, cvc5_sat)
+            if z3_sat == "unknown" or cvc5_sat == "unknown":
+                return FResult.SAFE, "unknown solve"
+            return FResult.ERROR, "Different SAT Results. Z3: {} CVC5: {}\nZ3:{}\nCVC5:{}".format(z3_sat, cvc5_sat,
+                                                                                                  z3_exit_code.stdout,
+                                                                                                  cvc_exit_code.stdout)
         elif (z3_sat != "" and cvc5_sat != "") and z3_sat == cvc5_sat:
             return FResult.SAFE, "its safe"
 
-        if z3_exit_code.returncode == 1:
+        # failed compilation
+        if z3_exit_code.returncode != 0 and cvc_exit_code.returncode != 0:
+            return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout+z3_exit_code.stderr,
+                                                                cvc_exit_code.stdout+cvc_exit_code.stderr)
+
+        if z3_exit_code.returncode != 0:
             if _check_error(cvc_exit_code.stdout):
-                return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout, cvc_exit_code.stdout)
+                return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout+z3_exit_code.stderr,
+                                                                    cvc_exit_code.stdout+cvc_exit_code.stderr)
             else:
-                return FResult.ERROR, "CVC5 is fine:\n{}\n but Z3 outputs:\n{}".format(cvc_exit_code.stdout,
-                                                                                       z3_exit_code.stdout)
-        elif cvc_exit_code.returncode == 1:
+                return FResult.ERROR, "CVC5 is fine:\n{}\n but Z3 outputs:\n{}"\
+                    .format(cvc_exit_code.stdout+cvc_exit_code.stderr,
+                            cvc_exit_code.stdout+cvc_exit_code.stderr)
+        elif cvc_exit_code.returncode != 0:
             if _check_error(z3_exit_code.stdout):
-                return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout, cvc_exit_code.stdout)
+                return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout+z3_exit_code.stderr,
+                                                                    cvc_exit_code.stdout+cvc_exit_code.stderr)
             else:
                 if _check_cvc5_parse_error(cvc_exit_code.stdout):
-                    return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout, cvc_exit_code.stdout)
-                return FResult.ERROR, "Z3 is fine:\n{}\n but CVC5 outputs:\n{}".format(z3_exit_code.stdout,
-                                                                                       cvc_exit_code.stdout)
+                    return FResult.FAILURE, "Z3:\n{}\nCVC5:\n{}".format(z3_exit_code.stdout+z3_exit_code.stderr,
+                                                                        cvc_exit_code.stdout+cvc_exit_code.stderr)
+                return FResult.ERROR, "Z3 is fine:\n{}\n but CVC5 outputs:\n{}"\
+                    .format(z3_exit_code.stdout+z3_exit_code.stderr,
+                            cvc_exit_code.stdout+cvc_exit_code.stderr)
 
         return FResult.SAFE, "its safe"
 
@@ -113,7 +130,7 @@ class SMTTarget(Target):
             f.write(clean_logic(code))
         try:
             exit_code = subprocess.run(
-                "cvc5-Linux --lang smt2 {}".format("temp{}.smt2".format(self.CURRENT_TIME)),
+                "cvc5 --lang smt2 {}".format("temp{}.smt2".format(self.CURRENT_TIME)),
                 shell=True, capture_output=True, text=True,
                 timeout=5)
             if exit_code.returncode == 0:
