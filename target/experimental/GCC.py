4b01bf4bf1dfd8d9024fcd746fe525417703b03b
steven <stevenxia990430@gmail.com>
2023-05-02 16:22:56 -0500

    re: moved base gcc to experimental

--- target/experimental/GCC.py
+++ target/experimental/GCC.py
@@ -1,49 +1,58 @@
 import subprocess
 
-from target.base_target import Target
-from target.base_target import FResult
+from target.base_target import FResult, Target
 
 
 class GCCTarget(Target):
-
     def validate_individual(self, filename) -> (FResult, str):
         self.logger.logo("Validating {} ...".format(filename))
 
-        exit_code = subprocess.run("gcc -c {} -o testbed/out".format(filename), shell=True,
-                                   capture_output=True,
-                                   encoding="utf-8",
-                                   text=True)
+        exit_code = subprocess.run(
+            "gcc -c {} -o testbed/out".format(filename),
+            shell=True,
+            capture_output=True,
+            encoding="utf-8",
+            text=True,
+        )
         if exit_code.returncode == 1:
             return FResult.FAILURE, "failed to compile"
         elif exit_code.returncode != 0:
             return FResult.ERROR, exit_code.stderr
 
-        for op in ['O1', 'O2', 'O3']:
-            exit_code = subprocess.run("gcc -{} -c {} -o testbed/out{}".format(op, filename, op), shell=True,
-                                       capture_output=True,
-                                       encoding="utf-8",
-                                       text=True)
+        for op in ["O1", "O2", "O3"]:
+            exit_code = subprocess.run(
+                "gcc -{} -c {} -o testbed/out{}".format(op, filename, op),
+                shell=True,
+                capture_output=True,
+                encoding="utf-8",
+                text=True,
+            )
             if exit_code.returncode != 0:
                 return FResult.ERROR, exit_code.stderr
 
         # check without -c option (+ linking)
-        exit_code = subprocess.run("gcc {} -o testbed/out".format(filename), shell=True,
-                                   capture_output=True,
-                                   encoding="utf-8",
-                                   text=True)
+        exit_code = subprocess.run(
+            "gcc {} -o testbed/out".format(filename),
+            shell=True,
+            capture_output=True,
+            encoding="utf-8",
+            text=True,
+        )
         if exit_code.returncode == 1:
             return FResult.FAILURE, "failed to compile"
         elif exit_code.returncode != 0:
             return FResult.ERROR, exit_code.stderr
 
-        for op in ['O1', 'O2', 'O3']:
-            exit_code = subprocess.run("gcc {} -{} -o testbed/out{}".format(filename, op, op), shell=True,
-                                       capture_output=True,
-                                       encoding="utf-8",
-                                       text=True)
+        for op in ["O1", "O2", "O3"]:
+            exit_code = subprocess.run(
+                "gcc {} -{} -o testbed/out{}".format(filename, op, op),
+                shell=True,
+                capture_output=True,
+                encoding="utf-8",
+                text=True,
+            )
             if exit_code.returncode != 0:
                 return FResult.ERROR, "{} failed to compile".format(op)
-
 
         # try:
         #     # TODO: maybe solve alot of user input related code, its causing many timeouts
@@ -93,4 +102,3 @@ class GCCTarget(Target):
         #             .format(op, base_code.stdout, op_code.stdout)
 
         return FResult.SAFE, "its safe"
-
