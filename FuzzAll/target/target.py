bbc48e0067a95a782e3b2795b8501e079de48207
steven <stevenxia990430@gmail.com>
2023-05-25 16:32:28 -0500

    fix: logger names

--- FuzzAll/target/target.py
+++ FuzzAll/target/target.py
@@ -24,8 +24,8 @@ class Target(object):
         self.folder = folder
         self.timeout = timeout
         # loggers
-        self.g_logger = Logger(self.folder, validation=True)
-        self.v_logger = Logger(self.folder, validation=False)
+        self.g_logger = Logger(self.folder, validation=False)
+        self.v_logger = Logger(self.folder, validation=True)
         self.CURRENT_TIME = time.time()
         # to be overwritten
         self.SYSTEM_MESSAGE = "You are a Fuzzer."
