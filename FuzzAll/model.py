605ba676902eba1b9c2d44103682165aee1c69df
mattepalte <mattepalte@live.it>
2023-06-20 17:07:39 +0200

    add (optional) CodeGen2 as local model

--- FuzzAll/model.py
+++ FuzzAll/model.py
@@ -140,5 +140,78 @@ class StarCoder:
         return outputs
 
 
+class CodeGen2:
+    def __init__(self, device="cuda", eos: List = None, max_length=3000) -> None:
+        checkpoint = "Salesforce/codegen2-7B"
+        self.device = device
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            checkpoint,
+            use_auth_token=AUTH_TOKEN,
+            # cache_dir=HF_CACHE_DIR
+        )
+        self.model = (
+            AutoModelForCausalLM.from_pretrained(
+                checkpoint,
+                use_auth_token=AUTH_TOKEN,
+                # cache_dir=HF_CACHE_DIR
+            )
+            .to(torch.bfloat16)
+            .to(device)
+        )
+        self.eos = EOF_STRINGS + eos
+        self.max_length = max_length
+        self.skip_special_tokens = False
+
+    def _construct_prompt(self, prompt):
+        # return prefix + "<mask_1>" + suffix + "<|endoftext|>" + "<sep>" + "<mask_1>"
+        return prompt + "<mask_1>" + "<|endoftext|>" + "<sep>" + "<mask_1>"
+
+    def generate(
+        self, prompt, batch_size=10, temperature=1.0, max_length=512
+    ) -> List[str]:
+        input_str = self._construct_prompt(prompt)
+        input_tokens = self.tokenizer.encode(input_str, return_tensors="pt").to(
+            self.device
+        )
+
+        scores = StoppingCriteriaList(
+            [
+                EndOfFunctionCriteria(
+                    start_length=len(input_tokens[0]),
+                    eos=self.eos,
+                    tokenizer=self.tokenizer,
+                )
+            ]
+        )
+
+        raw_outputs = self.model.generate(
+            input_tokens,
+            max_length=min(self.max_length, len(input_tokens[0]) + max_length),
+            do_sample=True,
+            top_p=1.0,
+            temperature=max(temperature, 1e-2),
+            num_return_sequences=batch_size,
+            stopping_criteria=scores,
+            output_scores=True,
+            return_dict_in_generate=True,
+            repetition_penalty=1.0,
+            pad_token_id=self.tokenizer.eos_token_id,
+        )
+        gen_seqs = raw_outputs.sequences[:, len(input_tokens[0]) :]
+        gen_strs = self.tokenizer.batch_decode(
+            gen_seqs, skip_special_tokens=self.skip_special_tokens
+        )
+        outputs = []
+        # removes eos tokens.
+        for output in gen_strs:
+            min_index = 10000
+            for eos in self.eos:
+                if eos in output:
+                    min_index = min(min_index, output.index(eos))
+            outputs.append(output[:min_index])
+        return outputs
+
+
 def make_model(eos: List = None):
     return StarCoder(eos=eos)
+    # return CodeGen2(eos=eos)
