ad0e4953731de757a5b6ddd8233f4ba0c7291a34
mattepalte <mattepalte@live.it>
2023-06-20 17:02:48 +0200

    document cmd line argument with help

--- FuzzAll/make_target.py
+++ FuzzAll/make_target.py
@@ -5,11 +5,36 @@ from FuzzAll.target.SMT.SMT import SMTTarget
 
 
 def make_target(args, parser):
-    parser.add_argument("--template", type=str, required=True)
-    parser.add_argument("--bs", type=int, required=True)
-    parser.add_argument("--temperature", type=float, required=True)
+    parser.add_argument(
+        "--template",
+        type=str,
+        required=True,
+        help="""
+        template file to use for the target.
+        Go to the main py file of the target (e.g. FuzzAll/target/CPP/CPP.py)
+        to see the template files which are supported for each target.
+    """,
+    )
+    parser.add_argument("--bs", type=int, required=True, help="batch size")
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        required=True,
+        help="temperature to query the LLM model when generating output.",
+    )
     parser.add_argument("--use_hw", action="store_true")
-    parser.add_argument("--prompt_strategy", type=int, required=True)
+    parser.add_argument(
+        "--prompt_strategy",
+        type=int,
+        required=True,
+        help="""
+            prompt strategy to generate obtain programs after the first one.
+                0: generate new code using separator
+                1: mutate existing code
+                2: semantically equivalent code generation
+                3: combine previous two code generations
+            """,
+    )
     args = parser.parse_args()
     if args.language == "cpp":  # GCC
         return args, CPPTarget(
